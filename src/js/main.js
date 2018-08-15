55f09a24f3ea756fc5f7a2dd51d7b4da03ff1bfd
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-08-15 20:47:54 -0300

    se agrega input y boton de tarea

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,16 @@
 function addList() {
   let newlist = document.createElement("div"); //div para lista de tareas
+  let newinput=document.createElement("input");
+  let newBtn=document.createElement("button")
   let texto = document.createTextNode("nueva lista");
-  newlist.appendChild(texto);
- 
+  newlist.appendChild(newinput);
+  newinput.appendChild(texto);
+  newlist.appendChild(newBtn);
+
   let list = document.getElementById("listas");
   list.appendChild(newlist);
+ 
+  
  
 
 }
